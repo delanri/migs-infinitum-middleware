03df9df95f29aeebd05b3924579f0ec4584c9814
gamma <gamma@lippox.com>
2017-05-02 15:59:36 +0700

    change vpcdps to vpcpay

--- routes/migsclient.js
+++ routes/migsclient.js
@@ -6,47 +6,21 @@ const _ = require('lodash')
 
 router.post('/', (req, res) => {
   const body = req.body
-  _.forEach(body, function(value, key){ if(value==='') delete body[key] })
-  
-  pay(body)
-  .then(response => {
-    console.log(response)
-    res.send(response)
+  let  url = 'https://migs.mastercard.com.au/vpcpay';
+  let form = `<form name="form" action="${url}" method="POST">`
+
+  _.forEach(body, function(value, key){
+    if(value === '') 
+      delete body[key]
+    else
+    {
+      form += `<input type="hidden" value="${value}" name="${key}"/>`
+    }
   })
-  .catch(error=>{
-    res.send(error)
-  })
+  form += '</form>'
+  form += '<script>document.form.submit()</script>'
+  res.send(form)
 
 })
-
-const pay = (payload) => {
-  const request = https.request
-  const options = {
-    host : 'migs.mastercard.com.au',
-    path : '/vpcdps',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    method: 'POST'
-  }
-
-  return new Promise((resolve, reject) => {
-    const req = request(options, (res) => {
-      let data = null;
-      res.on('data', (chunk) => {
-        data = chunk
-        console.log(`BODY: ${chunk}`)
-      })
-      res.on('end', () => res.statusCode != 200 ? reject(new Error()) : resolve(data))
-    })
-
-    req.on('socket', socket => {
-      socket.setTimeout(30000)
-      socket.on('timeout', () => req.abort())
-    })
-
-    req.on('err', (err) => reject(new Error()))
-    req.write(querystring.stringify(payload))
-    req.end()
-  })
-}
 
 module.exports = router
